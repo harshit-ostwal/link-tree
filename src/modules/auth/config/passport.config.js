fb95b4745abd760d67ebd43e83e7d220b19c21b3
Harshit Jain <harshitostwal1234@gmail.com>
2026-06-26 15:23:57 +0530

    feat: implement user status authorization middleware and remove deprecated auth files

--- src/modules/auth/config/passport.config.js
+++
@@ -1,9 +0,0 @@
-import GitHubStrategy from "../strategies/github.strategy.js";
-import GoogleStrategy from "../strategies/google.strategy.js";
-
-const passportInit = () => {
-  GoogleStrategy();
-  GitHubStrategy();
-};
-
-export default passportInit;
